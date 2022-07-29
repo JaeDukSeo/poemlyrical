f6ee90e0e91ebbdd3bdff51bb2a5ff2dbdab42b9
email <email@ryeron.ca>
2022-07-29 15:24:13 -0400

    'sds'

--- app.py
+++ app.py
@@ -410,7 +410,7 @@ def expand_node(sentence, past):
         input_ids = torch.tensor([sentence[-1]]).unsqueeze(0)
     inputs = {'input_ids': input_ids}    
     with torch.no_grad():
-        logits, past = model(**inputs, past=past, return_dict=False)
+        logits, past = model(**inputs, past_key_values =past, return_dict=False)
         #// use this line for Huggingface v3: logits, past = model(**inputs, past=past)
         logits[0][0][50256]=-math.inf # no <end of text> token
         logits = logits[:, -1, :]  
