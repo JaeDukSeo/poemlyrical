428323b9d14d25608565154914072bb2ea9108a7
email <email@ryeron.ca>
2022-07-29 14:19:15 -0400

    changed

--- app.py
+++ app.py
@@ -8,8 +8,28 @@ import math
 import pickle 
 import re
 
+# https://github.com/AlekseyKorshuk/huggingartists
+# List: https://huggingface.co/huggingartists
+
 st.title("Lyrical")
 name = st.text_input("Lyrics","")
 st.write("Output " + str(name))
 
+option = st.selectbox('Artist Name',(
+    'kendrick-lamar',
+    'kanye-west',
+    'nf',
+    'eminem',
+    'coldplay',
+    'drake',
+    'kota-the-friend',
+    'kanye-west',
+    'snoop-dogg',
+    'billie-eilish',
+    'logic',
+    'lil-uzi-vert',
+    'juice-wrld',
+    'snoop-dogg',
+    ))
+st.write('You selected:', option)
 
